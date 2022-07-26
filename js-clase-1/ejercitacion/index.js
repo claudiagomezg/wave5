44770bde713df7bf42277135ce76b9d1c5a67595
claudiagomezg <claudia.gomez@mercadolibre.cl>
2022-07-26 13:30:08 -0400

    ejercitacion 1 completa

--- js-clase-1/ejercitacion/index.js
+++ js-clase-1/ejercitacion/index.js
@@ -11,10 +11,64 @@ calculatePrice("play", 30000)
 output : "el costo de envio de una play es de 500 pesos y el precio total seria de 30500 pesos"
 *******************************************************************************/
 //Tu código acá
-let calculatePrice;
 
+// const calculatePrice = (productName, productPrice) => {
+// 	if(productPrice <= 0 ){
+// 		return "Error";
+// 	} else if(!productName || !productPrice){
+// 		return "ingresar ambos parámetros";
+// 	}else if(typeof productName !== 'string'){
+// 		return "ingresar un nombre válido";
+// 	}
+// 	let shippingPrice;
+// 	if(productPrice >= 1 && productPrice  <= 2000 ){
+// 		shippingPrice = 300;
+// 	}else if(productPrice >= 2001 && productPrice <= 4000){
+// 		shippingPrice = 500;
+// 	}else if(productPrice > 4000){
+// 		shippingPrice = 700;
+// 	}
+// 	return `El producto ${productName} cuesta ${productPrice}. Su costo de envío es de
+// 	$${shippingPrice}. Por lo tanto, el precio final es de ${productPrice + shippingPrice}`;
+// }
 
+// module.exports = {
+// 	calculatePrice,
+// };
 
-module.exports = {
+// // const calculatePrice = (producto, precio) => {
+// // 	let envio;
+// // 	if (precio <=0){
+// // 		return 'Error'
+// // 	} else if (producto == undefined || precio == undefined){
+// // 		return 'ingresar ambos parámetros'
+// // 	} else if (typeof producto != 'string'){
+// // 		return 'ingresar un nombre válido'
+// // 	} else if(precio > 0 && precio <= 2000){
+// // 		envio = 300
+// // 	} else if ( precio > 2000 && precio <= 4000){
+// // 		envio = 500
+// // 	} else if (precio > 4000){
+// // 		envio = 700
+// // 	} 
+
+// //  return `el producto ${producto} cuesta ${precio}. Su costo de envío es de ${envio}. Por lo tanto, el precio final es de ${precio + envio}`
+// // };
+const calculateEnvio = (price) => {
+	if (price <= 2000) return 300;
+	if (price >= 2001 && price <= 4000) return 500;
+	if (price > 4000) return 700;
+  };
+  const calculatePrice = (product, price) => {
+	if (price <= 0) return "Error";
+	if (!product || !price) return "ingresar ambos parámetros";
+	if (typeof product !== "string") return "ingresar un nombre válido";
+  
+	const envio = calculateEnvio(price);
+	const precioFinal = price + envio;
+	return `El producto ${product} cuesta ${price} . Su costo de envío es de ${envio} . Por lo tanto, el precio final es de ${precioFinal}`;
+  };
+  
+  module.exports = {
 	calculatePrice,
-};
+  };
